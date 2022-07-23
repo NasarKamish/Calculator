1d2cc534b3155d60ce47e0f5f845eee6dce1ed4d
Nasar <NasarKamish@gmail.com>
2022-07-23 14:17:16 +0200

    Updated file

--- index.js
+++ index.js
@@ -180,8 +180,6 @@ function action(key) {
     calculate();
   } else if (key in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) {
     // Numbers //
-    console.log(key);
-    console.log(value);
     if (key !== "0" && value !== "0") {
       value += key;
     } else if (key === "0" && value !== "0") {
